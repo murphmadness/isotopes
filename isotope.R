5e4e4303ee4f0d935fdd3b032aae65cb6aa07c31
Murphy <RiMurphy@nmslabs.com>
2020-10-06 12:40:21 -0400

    testing

---
+++ isotope.R
@@ -0,0 +1,19 @@
+###Testing isotopes as GIT repository
+
+library(openxlsx)
+
+
+
+
+
+
+
+
+
+a <- read.xlsx("//interface/FileXfer/COR/Metals-Alcohols/DataReview/isotope/NMS828/PAN2_2020_09_16_LC.xlsm",sheet="IsotopeRatio")
+
+#between this and looking at the 
+
+
+
+
